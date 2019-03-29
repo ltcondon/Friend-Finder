28c743430331959007541da271ba767ede90d8b5
Liam Condon <condonliamt@gmail.com>
2019-03-29 15:42:24 -0700

    test commit

--- server.js
+++ server.js
@@ -2,11 +2,15 @@
 
 var express = require("express");
 var app = express();
+var bodyParser = require("body-parser");
 
 var PORT = process.env.PORT || 3000;
 
 app.listen(PORT, function() {
     console.log("Server listening on port: " + PORT);
 });
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
 
 
