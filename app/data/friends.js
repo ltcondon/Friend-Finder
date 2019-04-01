2789bb76038eb16a3e6dbc64f8245f6f60da1196
Liam Condon <condonliamt@gmail.com>
2019-04-01 00:01:21 -0700

    added sterter form template using materialize

--- app/data/friends.js
+++ app/data/friends.js
@@ -2,32 +2,32 @@ var friendsArray = [
     {
         name: "Joe Smith",
         photo: "#",
-        answers: [4,2,5,3,9,5,6,4,3,2]
+        answe: [4,2,5,3,9,5,6,4,3,2]
     },
     {
         name: "Jon Smith",
         photo: "#",
-        answers: [1,2,5,3,2,5,6,3,3,2]
+        answe: [1,2,5,3,2,5,6,3,3,2]
     },
     {
         name: "Al Smith",
         photo: "#",
-        answers: [4,1,5,3,2,5,6,4,3,2]
+        answe: [4,1,5,3,2,5,6,4,3,2]
     },
     {
         name: "Jake Smith",
         photo: "#",
-        answers: [4,5,5,3,2,5,6,6,3,2]
+        answe: [4,5,5,3,2,5,6,6,3,2]
     },
     {
         name: "Pete Smith",
         photo: "#",
-        answers: [3,3,3,3,2,5,6,4,3,2]
+        answe: [3,3,3,3,2,5,6,4,3,2]
     },
     {
         name: "Mike Smith",
         photo: "#",
-        answers: [4,5,5,3,8,9,9,5,1,2]
+        answe: [4,5,5,3,8,9,9,5,1,2]
     },
 ]
 
