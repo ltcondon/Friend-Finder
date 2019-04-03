71a56a8e196421bf9d27ae648e15075c313e8bc6
Liam Condon <condonliamt@gmail.com>
2019-04-02 21:36:45 -0700

    broke out neighborhoods array into seperate js file

---
+++ app/data/neighborhoods.js
@@ -0,0 +1,32 @@
+var neighborhoods = [
+    {
+        name: "East Village",
+        bio: "What was once the pulse of NYC’s hippie/artist scene (have you seen Rent? Same, same.) has moved aside as NYU students and faculty have taken over. Still, it’s one of the best neighborhoods in NYC for culture, nightlife, and dining... if you don’t mind dining next to college kids.",
+        quote: "“The amount of food choices is crippling, in a good way. But don’t be surprised when the bro-next-door leaves a trail of pizza to your door in a boozy state and then vomits all over the floor... and then tries to clean it up with a slice of pizza.”",
+        answers: [1,1,1,1,1],
+        image: "https://www.triplemint.com/blog/wp-content/uploads/2017/11/East-Village-Feature-759x500.jpg"
+    },
+    {
+        name: "Upper East Side",
+        bio: "A mixed bag of “you will never be able to afford this,” and “oh wow, this is surprisingly affordable.” And you’re walking distance to the Metropolitan Museum of Art, the Guggenheim, and the Frick. Oh and consulates. ALL THE CONSULATES... which is great if you, like... need a visa.",
+        quote: "“Sure, it's sprinkled with your generic frat boy pubs and you may have to play chicken with a few double-wide strollers, but the Upper East has an unexpectedly homey neighborhood vibe with great grocery stores (a must for me) and easy access to Central Park and museums -- plus, a few of my favorite Downtown restaurants have even migrated north!”",
+        answers: [2,2,2,2,2],
+        image: "https://moneydotcomvip.files.wordpress.com/2015/07/150709_rea_uesnyc2.jpg"
+    },
+    {
+        name: "Sunnyside",
+        bio: "And as Astoria manages to fall victim to rent hikes, we present Sunnyside. Bursting with international culture and beauty, including but not limited to Paraguayan, Filipino, and Indian, Sunnyside continues to pull in young professionals (and serious foodies) seeking more of a diverse scene, who want more bang for their buck.",
+        quote: "“As someone who loves eating, Sunnywide is perfect because it conveniently has three large supermarkets contained within about seven blocks, and a smorgasbord of ethnic restaurants and more places are moving in all the time.”",
+        answers: [3,3,3,3,3],
+        image: "https://www.tracysnewyorklife.com/wp-content/uploads/2017/06/DSC07366-800x533.jpg"
+    },
+    {
+        name: "Bushwick",
+        bio: "You're an artist...got it. A popular hipster enclave in deep Brooklyn, Bushwick combines a thriving art, restaurant and bar scene with an older, more entrenched Hispanic culture. The avenues have a lot of art galleries, boutiques, restaurants and bars, largely because of the proximity of the L train. The area north of Myrtle Avenue has much more industry and a lot of heavy truck traffic.",
+        quote: "“This is a place where street parking is prime, and yet the day I was moving in, somoene said, ‘Would you like me to move my car so we could fit the truck in there?’ And that says a lot about a community.”",
+        answers: [4,4,4,4,4],
+        image: "https://static01.nyt.com/images/2016/06/19/realestate/19LIVING-BUSHWICK-slide-7ZIS/19LIVING-BUSHWICK-slide-7ZIS-superJumbo.jpg"
+    },
+    
+]
+module.exports = neighborhoods;
--- server.js
+++ server.js
@@ -1,20 +1,26 @@
 // ======== Require node packages ============
 
 var express = require("express");
+var path = require("path");
+
 var app = express();
-// var bodyParser = require("body-parser");
 
-
+// Set up PORT:
 var PORT = process.env.PORT || 3000;
 
+// Initialize Server:
 app.listen(PORT, function() {
     console.log("Server listening on port: " + PORT);
 });
 
+// Parse and handle incoming data:
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 
-app.use(express.static('public'));
+// Provide server access to my public directory for serving my css and images
+app.use(express.static(path.join(__dirname, "./app/public")));
+app.use(express.static(path.join(__dirname, "./app/public/assets")));
 
+// Require HTML and API routing files:
 require("./app/routing/htmlRoutes")(app);
 require("./app/routing/apiRoutes")(app);
